5fbdcb689ed28175a818b03e57074e7eaecb8d96
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-04-29 02:02:33 -0500

    First Commit

---
+++ components/Account.js
@@ -0,0 +1,124 @@
+import { useState, useEffect } from "react";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import Avatar from "./Avatar";
+
+export default function Account({ session }) {
+  const supabase = useSupabaseClient();
+  const user = useUser();
+  console.log("USER", user);
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState(null);
+  const [fullName, setFullName] = useState(null);
+  const [avatarUrl, setAvatarUrl] = useState(null);
+
+  useEffect(() => {
+    getProfile();
+  }, [session]);
+
+  async function getProfile() {
+    try {
+      setLoading(true);
+
+      let { data, error, status } = await supabase
+        .from("profiles")
+        .select(`username, full_name, avatar_url`)
+        .eq("id", user.id)
+        .single();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setUsername(data.username);
+        setFullName(data.full_name);
+        setAvatarUrl(data.avatar_url);
+      }
+    } catch (error) {
+      alert("Error loading user data!");
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function updateProfile({ username, full_name, avatar_url }) {
+    try {
+      setLoading(true);
+
+      const updates = {
+        id: user.id,
+        username : username,
+        full_name,
+        avatar_url,
+        updated_at: new Date().toISOString(),
+      };
+
+      let { error } = await supabase.from("profiles").upsert(updates);
+      if (error) throw error;
+      alert("Profile updated!");
+    } catch (error) {
+      alert("Error updating the data!");
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="form-widget">
+    {fullName ? <h1>Welcome {fullName}</h1>: username ? <h1>Welcome {username } !!</h1> : <h1>Welcome!!</h1>}    
+
+      <Avatar
+        uid={user.id}
+        url={avatarUrl}
+        size={150}
+        onUpload={(url) => {
+          setAvatarUrl(url);
+          updateProfile({ username, full_name: fullName, avatar_url: url });
+        }}
+      />
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" value={session.user.email} disabled />
+      </div>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={username || ""}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="full_name">Full Name</label>
+        <input
+          id="full_name"
+          type="text"
+          value={fullName || ""}
+          onChange={(e) => setFullName(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <button
+          className="button primary block"
+          onClick={() => updateProfile({ username: username, full_name: fullName, avatar_url: avatarUrl })}
+          disabled={loading}
+        >
+          {loading ? "Loading ..." : "Update"}
+        </button>
+      </div>
+
+      <div>
+        <button
+          className="button block"
+          onClick={() => supabase.auth.signOut()}
+        >
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
+}
