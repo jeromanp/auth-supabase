68b7e59c704eb0bde4d492bfffc3515ee512cda8
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-05-01 02:32:01 -0500

    Object USER in session

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,20 @@
 import { supabase } from "../utils/supabase";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { useSession } from "@supabase/auth-helpers-react";
+import { useUser } from "@supabase/auth-helpers-react";
 
 export default function Home({ lessons }) {
+  const user = useUser();
+  console.log({ user });
 
-  const session = useSession()
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    session
-    console.log(session);
-  }, [isLoggedIn]);
   return (
     <div className="w-full max-w-3xl mx-auto my-16 px-2">
       {lessons.map((lesson) => (
-        <Link key={lesson.id} href={`/${lesson.id}`} passHref={true} legacyBehavior={true}>
+        <Link
+          key={lesson.id}
+          href={`/${lesson.id}`}
+          passHref={true}
+          legacyBehavior={true}
+        >
           <a className="p-8 h-40 mb-4 rounded shadow text-xl flex">
             {lesson.title}
           </a>
